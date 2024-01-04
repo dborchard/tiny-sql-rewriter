07bacb1dfdd00e4485aef34f163bc9670951fbfc
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-01-04 13:31:31 -0800

    suggestion done.

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,17 @@ import (
 )
 
 func main() {
-	sql := "select * from t where id = 1"
+	sql := "select * from tbl t1 where id < 1000"
 
 	// 1. suggest optimizations
 	heuristicSuggest := suggestOptimizations(sql)
 	fmt.Println(heuristicSuggest)
+	/*
+		map[
+			ALI.001: {ALI.001 L0 It is recommended to use the AS keyword to explicitly declare an alias In column or table aliases (such as "tbl AS alias"), explicit use of the AS keyword is more understandable than implicit aliases (such as "tbl alias").  select name from tbl t1 where id < 1000 0 0x10327f9e0}
+			OK: {OK L0 OK OK OK 0 0x10327f920}
+		]
+	*/
 
 	// 2. rewrite sql
 	newSql := rewriteSql(sql)
